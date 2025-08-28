cc6655eb1b23ce61f119e5e40dd468b599587885
Alfonso <alfonso_miguel@hotmail.com>
2025-08-28 23:35:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,26 +14,27 @@ st.title("🚧 Endplate Design Criteria Predictor")
 st.write("Upload an Excel file with new endplate design data to predict if designs are acceptable.")
 
 # File uploader
-#uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
+uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
 
-#if uploaded_file:
+if uploaded_file:
     # Read Excel
-    #new_data= pd.read_excel('new_input_HPC_RIS_DN150.xlsx')
-    #new_data = new_data.drop(columns =['symmetry','item'])
-    #new_data['collapse'] = new_data['collapse'].replace({1: 'yes', 0: 'no'})
+    new_data = pd.read_excel(uploaded_file)
+    new_data = new_data.drop(columns =['symmetry','item'])
+    new_data['collapse'] = new_data['collapse'].replace({1: 'yes', 0: 'no'})
     
-    #st.subheader("📂 Uploaded Data Preview")
-    #st.dataframe(new_data.head())
+    st.subheader("📂 Uploaded Data Preview")
+    st.dataframe(new_data.head())
 
     # Make predictions
-    #preds = model.predict(new_data)
+    preds = model.predict(new_data)
 
     # Add predictions to dataframe
-    #new_data["Predicted_Design_Criteria"] = preds
-    #new_data["Status"] = ["✅ Acceptable" if p < 1 else "❌ Not Acceptable" for p in preds]
+    new_data["Predicted_Design_Criteria"] = preds
+    new_data["Status"] = ["✅ Acceptable" if p < 1 else "❌ Not Acceptable" for p in preds]
 
-    #st.subheader("🔮 Predictions")
+    st.subheader("🔮 Predictions")
 
-    #st.dataframe(new_data)
+    st.dataframe(new_data)
+
 
 
